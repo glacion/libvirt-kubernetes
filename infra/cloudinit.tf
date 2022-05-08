a28f9281d108125e8079b8c2a138495b5f8ee733
Can Güvendiren <ahmtcngvndrn@gmail.com>
2022-05-08 20:45:53 +0300

    feat(infra): remove instance resources

--- infra/cloudinit.tf
+++
@@ -1,9 +0,0 @@
-resource "libvirt_cloudinit_disk" "init" {
-  name      = "commoninit.iso"
-  user_data = file("${path.module}/cloud-init.yml")
-	meta_data = jsonencode({
-		instance-id    = "fkjdkjfajksdlfjkasjd"
-		local-hostname = "master.glacion.com"
-	}) 
-}
-
